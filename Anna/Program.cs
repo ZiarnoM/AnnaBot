bec211f1c7966d3856446d9aac85af82f8065b3b
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2022-03-17 11:20:12 +0100

    fix unwanted changes

--- Anna/Program.cs
+++ Anna/Program.cs
@@ -6,19 +6,10 @@ namespace Anna
     {
         static void Main(string[] args)
         {
-            var config = ConfigModel.DeserializeData("config.json");
+            var config = ConfigModel.DeserializeData("../../../config.json");
             var db = new Db(config);
-            Message msg = new Message();
-            msg.command = "deploy";
-            msg.parameters = new string[]
-            {
-                "anna",
-                "C:/Users/KDR Praktyki/repo/AnnaBot",
-                "main",
-                "C:/Users/KDR Praktyki/repo/AnnaBot5",
-                "-o publish3"
-            };
-            CommandRunner.DetectAndRunComamandFunction(msg);
+            IrcBot ircbot = new IrcBot(config);
+            ircbot.Run();
         }
     }
 }
