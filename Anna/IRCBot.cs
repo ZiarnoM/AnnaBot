cd2d5bfdf2523a97b0bae1093f9aa6559a51a3f1
Michal Zarnowski <zar.michal@wp.pl>
2022-03-22 11:38:56 +0100

    fix sendmessage

--- Anna/IRCBot.cs
+++ Anna/IRCBot.cs
@@ -15,9 +15,9 @@ namespace Anna
     public class Message
     {
         public string command { get; set; }
-        
+
         public string[] parameters { get; set; }
-        
+
         public string sender { get; set; }
         public Boolean isFlag { get; set; }
     }
@@ -54,7 +54,6 @@ namespace Anna
                     {
                         hold += c;
                     }
-
                 }
                 else if (inargp)
                 {
@@ -85,6 +84,7 @@ namespace Anna
                     }
                 }
             }
+
             if (hold.Length > 0)
             {
                 data.Add(hold);
@@ -99,20 +99,19 @@ namespace Anna
             {
                 isFlag = false;
             }
-            Message messageContent = new Message { command = data[0], parameters = data.Skip(1).ToArray(),sender = sender, isFlag = isFlag };
+
+            Message messageContent = new Message
+                {command = data[0], parameters = data.Skip(1).ToArray(), sender = sender, isFlag = isFlag};
             return messageContent;
         }
 
-        public void sendMessage(StreamWriter writer, string receiver, string[] messages)
+        public void sendMessage(StreamWriter writer, string receiver, string message)
         {
-            foreach (string message in messages)
-            {
-                string[] args = new string[] { _config.nick, message, receiver };
-                CommandRunner.SqlInsertLog(args);
-                writer.WriteLine($"PRIVMSG {receiver} :{message}");
-            }
-            
+            string[] args = new string[] {_config.nick, message, receiver};
+            CommandRunner.SqlInsertLog(args);
+            writer.WriteLine($"PRIVMSG {receiver} :{message}");
         }
+
         public void Run()
         {
             using (var client = new TcpClient())
@@ -122,13 +121,13 @@ namespace Anna
                 Console.WriteLine($"Connected: {client.Connected}");
 
                 //log bot initiation
-                object[] upTimeArgs = { _config.nick, "!BOTSTARTED!", 0 , 0};
+                object[] upTimeArgs = {_config.nick, "!BOTSTARTED!", 0, 0};
 
                 CommandRunner.SqlInsertSystemLog(upTimeArgs);
-                
+
                 // String URLString = "https://www.polsatnews.pl/rss/biznes.xml";
                 // XmlTextReader readerXml = new XmlTextReader (URLString);
-                
+
                 // {
                 //     switch (readerXml.NodeType)
                 //     {
@@ -185,54 +184,56 @@ namespace Anna
                                     case "376":
                                     case "422":
 
+                                    {
+                                        foreach (string channel in _config.channels)
                                         {
-                                            foreach (string channel in _config.channels)
-                                            {
-                                                writer.WriteLine($"JOIN {channel}");
-                                                // communicate with everyone on the channel as soon as the bot logs in
-                                                sendMessage(writer, channel, new []{ CommandRunner.GetCodesValue("WELCOME-MESSAGE")});
-                                                writer.Flush();
-                                            }
-
-                                            break;
+                                            writer.WriteLine($"JOIN {channel}");
+                                            // communicate with everyone on the channel as soon as the bot logs in
+                                            sendMessage(writer, channel,
+                                                CommandRunner.GetCodesValue("WELCOME-MESSAGE"));
+                                            writer.Flush();
                                         }
+
+                                        break;
+                                    }
                                     case "PRIVMSG":
+                                    {
+                                        if (d.Length > 2)
                                         {
-                                            if (d.Length > 2)
+                                            if ((d[2] == _config.nick) || (_config.channels.Contains(d[2])))
                                             {
-                                                if ((d[2] == _config.nick) || (_config.channels.Contains(d[2])))
+                                                var sender = data.Split('!')[0].Substring(1);
+                                                string[] messageList = data.Split(':')[2..];
+                                                string message = String.Join(":", messageList).Trim();
+                                                Console.WriteLine($"Message: {message}");
+
+                                                //private message case
+                                                if (d[2] == _config.nick)
                                                 {
-                                                    var sender = data.Split('!')[0].Substring(1);
-                                                    string[] messageList = data.Split(':')[2..];
-                                                    string message = String.Join(":", messageList).Trim();
-                                                    Console.WriteLine($"Message: {message}");
-
-                                                    //private message case
-                                                    if (d[2] == _config.nick)
-                                                    {
-                                                        //change destination to sender nick
-                                                        d[2] = sender;
-                                                    }
-                                                    //channel message
-                                                    else
-                                                    {
-                                                        string[] args = new string[] { sender, message, d[2] };
-                                                        CommandRunner.SqlInsertLog(args);
-                                                    }
-
-                                                    if (message[0] == '!')
-                                                    {
-                                                        sendMessage(writer, d[2], new []{ CommandRunner.DetectAndRunComamandFunction(checkMessage(sender, message))});
-                                                    }
-
-                                                    writer.Flush();
+                                                    //change destination to sender nick
+                                                    d[2] = sender;
+                                                }
+                                                //channel message
+                                                else
+                                                {
+                                                    string[] args = new string[] {sender, message, d[2]};
+                                                    CommandRunner.SqlInsertLog(args);
                                                 }
 
+                                                if (message[0] == '!')
+                                                {
+                                                    sendMessage(writer, d[2],
+                                                        CommandRunner.DetectAndRunComamandFunction(checkMessage(sender,
+                                                            message)));
+                                                }
+
+                                                writer.Flush();
                                             }
-
-
-                                            break;
                                         }
+
+
+                                        break;
+                                    }
                                 }
                             }
                         }
