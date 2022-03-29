17f5bd405f335c10548b3bdb295b80feb9a8897d
wiktor-wektor <wiktor.gorka55@gmail.com>
2022-03-29 13:04:00 +0200

    add date form upgrade controllers

--- AnnaLogs/AnnaLogs/Controllers/ALogsController.cs
+++ AnnaLogs/AnnaLogs/Controllers/ALogsController.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using System;
 
 namespace AnnaLogs.Controllers
 {
@@ -20,25 +21,34 @@ namespace AnnaLogs.Controllers
         {
             _ = asd;
             string testing = asd;
-            string res = asd.Substring(0, 1);
-            return All(testing);
+            return ViewLog(testing,"");
         }
-        public IActionResult All(string channLog)
+
+        public IActionResult ViewLog(string channLog,string datLog)
+        {
+            string DateTimeLog = datLog;
+            return All(channLog, DateTimeLog);
+        }
+        public IActionResult All(string channLog, string date)
         {
             string tempVar = channLog;
-            DataRow x = Db.ExecSql("Select top 1 convert(varchar(10), Date, 104) from Log Order By Date DESC;", new object[] { });
+            DataRow x = Db.ExecSql("Select top 1 convert(varchar(10), Date, 120) from Log Order By Date DESC;", new object[] { });
             string nn = @x.ItemArray[0].ToString();
             string[] n = nn.Split(' ');
-            DataRowCollection Values = Db.ExecSqlCollection("Select Id,UserNick,Message,Date,Channel from Log where Channel = @p0;", new object[] { tempVar });
-            DataRowCollection Dates = Db.ExecSqlCollection("Select Distinct convert(varchar(10), Date, 104) from Log;", new object[] { });
+            if (date == "")
+            {
+                date = n[0];
+            }
+            DataRowCollection Values = Db.ExecSqlCollection("Select Id,UserNick,Message,Date,Channel from Log where (Channel = @p0) and (Left(convert(varchar(10), Date, 120), 10) =@p1);", new object[] { tempVar, date });
+            DataRowCollection Dates = Db.ExecSqlCollection("Select Distinct convert(varchar(10), Date, 120) from Log;", new object[] { });
             ALogsViewModel m = new ALogsViewModel()
             {
                 Logs = Values,
                 UniqeDates = Dates,
                 channelName = tempVar,
-                Last = n[0]
+                Last = date
             };
-            return View("All",m);
+            return View("All", m);
         }
     }
 }
